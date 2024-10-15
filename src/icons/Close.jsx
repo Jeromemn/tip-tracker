d60fb4080f6f780c3188523a8ef4c18a90df33a4
nixon <jeromenixon95@gmail.com>
2024-10-15 10:47:43 -0700

    added mobile menu for navigation

--- src/icons/Close.jsx
+++ src/icons/Close.jsx
@@ -1,4 +1,4 @@
-const Close = () => (
+const Close = ({ color }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
@@ -8,7 +8,7 @@ const Close = () => (
   >
     <g clipPath="url(#a)">
       <path
-        fill="#252422"
+        fill={color}
         d="M24 1.414 22.586 0 12 10.586 1.414 0 0 1.414 10.586 12 0 22.586 1.414 24 12 13.414 22.586 24 24 22.586 13.414 12 24 1.414Z"
       />
     </g>
